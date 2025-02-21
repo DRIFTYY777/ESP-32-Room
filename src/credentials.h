c416615e38564aa22bd8061e3c96e876e58016ca
DRIFTYY777 <93861763+DRIFTYY777@users.noreply.github.com>
2025-02-21 19:17:09 +0530

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -4,8 +4,8 @@
 
 #define API_KEY "AIzaSyBfppxm4qqUo60xeV2k7Ztr9z2qHc0lT6I"
 #define FIREBASE_PROJECT_ID "that-esp"
-#define USER_EMAIL "gbjhasd344@gmail.com"
-#define USER_PASSWORD "mmmmmmmmmm777"
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
 
 #define SERVER_KEY ""
 
