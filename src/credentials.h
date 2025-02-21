eabef0c503eba31a8bd3f7cc5c21dfe8664ccf39
DRIFTYY777 <93861763+DRIFTYY777@users.noreply.github.com>
2025-02-21 19:17:19 +0530

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -2,7 +2,7 @@
 #define __CREDENTIALS_H__
 
 
-#define API_KEY "AIzaSyBfppxm4qqUo60xeV2k7Ztr9z2qHc0lT6I"
+#define API_KEY ""
 #define FIREBASE_PROJECT_ID "that-esp"
 #define USER_EMAIL ""
 #define USER_PASSWORD ""
